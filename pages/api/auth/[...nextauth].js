5ae553ab42c507a52dc5f1c061afc742699442f4
Rox Works <dsg6874@g.rit.edu>
2022-04-28 01:03:14 -0600

    cleaned up

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -35,10 +35,8 @@ export default NextAuth({
         async jwt({ token, account }) {
             // Persist the OAuth access_token to the token right after signin
             if (account) {
-                let newToken = await refreshTwitchAccessToken(account.access_token, account.refresh_token);
-                console.log('GOT TOKEN: ', newToken);
-                token.accessToken = newToken.access_token
-                token.refreshToken = newToken.refresh_token
+                token.accessToken = account.access_token
+                token.refreshToken = account.refresh_token
             }
             return token
         },
