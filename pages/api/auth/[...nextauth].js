5f3a1b5fa9d52bbc4b4c9c94ba97357ea7dc8446
Rox Works <dsg6874@g.rit.edu>
2022-04-28 00:03:22 -0600

    pls

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,18 @@
 import NextAuth from "next-auth"
 import TwitchProvider from "next-auth/providers/twitch"
+import axios from 'axios'
+
+const refreshTwitchAccessToken = async (accessToken, refreshToken) => {
+    const response = await axios.post('https://id.twitch.tv/oauth2/token', {
+        client_id: process.env.TWITCH_CLIENT_ID,
+        client_secret: process.env.TWITCH_CLIENT_SECRET,
+        grant_type: 'refresh_token',
+        refresh_token: refreshToken
+    });
+    console.log(`refreshTokenResponse: ${JSON.stringify(response.data)}`);
+    return response.data;
+}
+
 
 export default NextAuth({
     // Configure one or more authentication providers
@@ -23,12 +36,14 @@ export default NextAuth({
             // Persist the OAuth access_token to the token right after signin
             if (account) {
                 token.accessToken = account.access_token
+                token.refreshToken = account.refresh_token
             }
             return token
         },
         async session({ session, token }) {
             // Send properties to the client, like an access_token from a provider.
             session.accessToken = token.accessToken
+            session.refreshToken = token.refreshToken
             return session
         }
     }
